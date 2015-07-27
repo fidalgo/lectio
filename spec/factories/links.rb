eb24356d93f3beee415748ca40e7221af512d1c3
Paulo Fidalgo <fidalgo@gmx.com>
2015-07-27 17:21:23 +0100

    Add tests for mark as read/unread in Links.

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :link do
     sequence(:url,200000){ |n| "https://bugs.kde.org/show_bug.cgi?id={n}" }
+    read false
+    # association :user, factory: :user, strategy: :build
   end
 
 end
--- spec/features/links_spec.rb
+++ spec/features/links_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
+require 'pry-byebug'
 
 feature "Displaying Saved Links" do
 
   background do
-    user = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user)
     visit "/users/sign_in"
-    fill_in "user_email", with: user.email
-    fill_in "user_password", with: user.password
+    fill_in "user_email", with: @user.email
+    fill_in "user_password", with: @user.password
     click_button "Log in"
     expect(page).to have_content("Signed in successfully.")
   end
@@ -25,6 +26,24 @@ feature "Displaying Saved Links" do
     visit "/links"
     click_link "CGI: Ruby's Bare Metal"
     expect(page).to have_text("CGI: Ruby's Bare Metal")
+  end
+
+  scenario "User marks link as read" do
+    link = FactoryGirl.create(:link, :user => @user)
+    visit read_link_path(link)
+    link = Link.find(link.id)
+    expect(link.read).to eq(true)
+  end
+
+  scenario "User marks link as unread" do
+    link = FactoryGirl.create(:link, :user => @user)
+    expect(link.read).to eq(false)
+    visit read_link_path(link)
+    link = Link.find(link.id)
+    expect(link.read).to eq(true)
+    visit read_link_path(link)
+    link = Link.find(link.id)
+    expect(link.read).to eq(false)
   end
 
 end
