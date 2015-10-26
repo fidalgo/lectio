2840164ad65565c4ec6148ffb852bfe3252746b3
Paulo Fidalgo <fidalgo@gmx.com>
2015-10-26 22:53:30 +0000

    Add an Help section with basic instructions.

---
+++ spec/features/visitors/help_page_spec.rb
@@ -0,0 +1,14 @@
+# Feature: 'Help' page
+#   As a visitor
+#   I want to visit an 'help' page
+#   So I can learn more help the website
+feature 'Help page' do
+  # Scenario: Visit the 'help' page
+  #   Given I am a visitor
+  #   When I visit the 'help' page
+  #   Then I see "Help the Website"
+  scenario 'Visit the help page' do
+    visit 'pages/help'
+    expect(page).to have_content 'Help the Website'
+  end
+end
