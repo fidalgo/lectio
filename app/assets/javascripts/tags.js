919eaa1be0b9cc29fd6045e49cb3054eb5776a9f
Paulo Fidalgo <fidalgo@gmx.com>
2015-11-12 18:48:43 +0000

    Streamline the assets
    Put the tag input code initialization in a new file.
    Fix the bootstrap theme for select2.

---
+++ app/assets/javascripts/tags.js
@@ -0,0 +1,32 @@
+$(document).on('page:change', function() {
+  $("#tags-input").select2({
+    tags: true,
+    width: '100%',
+    tokenSeparators: [",", " "],
+    ajax: {
+      dataType: 'json',
+      url: '/links/tags',
+      data: function(params) {
+        return {
+          query: params.term,
+          page: params.page
+        };
+      },
+      processResults: function(data, params) {
+        params.page = params.page || 1;
+        return {
+          results: $.map(data, function(tag) {
+            return {
+              id: tag.name,
+              text: tag.name
+            };
+          }),
+          pagination: {
+            more: (params.page * 30) < data.total_count
+          }
+        };
+      },
+      cache: true
+    }
+  });
+});
