ff4074368d8c5abaf12b1ae3f151a94212fdbcbb
Paulo Fidalgo <fidalgo@gmx.com>
2015-11-12 21:24:17 +0000

    Add view and controller tag support
    Also add a new method to search for user tags, allowing the
    autocompletion in the from, via AJAX.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -8,10 +8,15 @@ class LinksController < ApplicationController
     @links = current_user.links.order(read: :asc).order(created_at: :desc).page params[:page]
   end
 
-  # GET /links/1
-  # GET /links/1.json
-  # def show
-  # end
+  # GET /tags/#{query}.json
+  def tags
+    if params[:query].present?
+      # TODO: Check for possible SQLi here
+      render json: current_user.tags.where('name LIKE ?', "%#{params[:query]}%")
+    else
+      render json: current_user.tags
+end
+end
 
   def read
     link.read = link.read ? false : true
@@ -38,9 +43,9 @@ class LinksController < ApplicationController
   def create
     @link = Link.new(link_params)
     link.user = current_user
-
     respond_to do |format|
       if link.save
+        current_user.tag(@link, link_params['tags_list'])
         UrlScrapperJob.perform_later @link.id
         format.html { redirect_to links_url, notice: 'Link was successfully created.' }
         format.json { render :show, status: :created, location: @link }
@@ -54,8 +59,10 @@ class LinksController < ApplicationController
   # PATCH/PUT /links/1
   # PATCH/PUT /links/1.json
   def update
+    logger.info "TAGS: #{@link.tags}"
     respond_to do |format|
       if link.update(link_params)
+        current_user.tag(@link, link_params['tags_list'])
         UrlScrapperJob.perform_later @link.id
         format.html { redirect_to links_url, notice: 'Link was successfully updated.' }
         format.json { render :show, status: :ok, location: @link }
@@ -84,6 +91,6 @@ class LinksController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def link_params
-    params.require(:link).permit(:url, :title)
+    params.require(:link).permit(:url, :title, :read, tags_list: [])
   end
 end
