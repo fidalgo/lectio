d09ee436ecd17d026724db6cc08ad02148a30232
Paulo Fidalgo <fidalgo@gmx.com>
2015-11-13 00:10:49 +0000

    Improve the URL Scrapper
    Get the title by searching and the description from either
    the name or the property.

--- app/jobs/url_scrapper_job.rb
+++ app/jobs/url_scrapper_job.rb
@@ -2,23 +2,22 @@
 class UrlScrapperJob < ActiveJob::Base
   queue_as :default
 
-# TODO: Add ruby-readability when we have no description
   def perform(link_id)
     link = Link.find(link_id)
     response = HTTParty.get(link.url)
     page = Nokogiri::HTML(response.body, nil, 'UTF-8')
-    title = page.css('title').text.strip!
-    description = page.xpath("//meta[case_insensitive_include(@name, 'description')]/@content",
-    XpathFunctions.new).text
+    title = page.search('title').inner_text
+    logger.info "URL: #{link.url} Title: #{title}"
+    description = page.xpath("//meta[case_insensitive_include(@name, 'description')
+      or case_insensitive_include(@property, 'description')]/@content",
+                             XpathFunctions.new).text
     link.update(title: title, description: description)
   end
 end
 
-
+# Allow to seach with insensitive case
 class XpathFunctions
-
   def case_insensitive_include(node_set, str_to_match)
-    node_set.find_all {|node| node.to_s.downcase.include? str_to_match.to_s.downcase }
+    node_set.find_all { |node| node.to_s.downcase.include? str_to_match.to_s.downcase }
   end
-
 end
