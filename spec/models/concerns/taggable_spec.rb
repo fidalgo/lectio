ac355ad3d8196c9d3003c8f32a1066f0f5e00135
Paulo Fidalgo <fidalgo@gmx.com>
2016-02-11 23:38:20 +0000

    Add specs for the taggable concern,

---
+++ spec/models/concerns/taggable_spec.rb
@@ -0,0 +1,23 @@
+shared_examples_for 'taggable' do
+  let(:model) { build(described_class.to_s.underscore) } # the class that includes the concern
+
+  it 'has many taggings' do
+    expect(model).to have_many(:taggings).dependent(:destroy)
+  end
+
+  it 'has many tags' do
+    expect(model).to have_many(:tags).through(:taggings)
+  end
+
+  describe '#tags_list' do
+    it 'lists the tag names' do
+      ruby_tag = build(:tag, name: 'ruby')
+      rails_tag = build(:tag, name: 'rails')
+      user = build(:user)
+      taggable = build(described_class.to_s.underscore)
+      create(:tagging, tag: ruby_tag, taggable: taggable, tagger: user)
+      create(:tagging, tag: rails_tag, taggable: taggable, tagger: user)
+      expect(taggable.tags_list.size).to eq(2)
+    end
+  end
+end
