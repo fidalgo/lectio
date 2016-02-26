d1fc4fce72ed00948f03cf31da49a576fed5d42f
Paulo Fidalgo <fidalgo@gmx.com>
2016-02-26 18:25:26 +0000

    Fix the factory link creation by setting the title to nil.

--- spec/jobs/url_scrapper_job_spec.rb
+++ spec/jobs/url_scrapper_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UrlScrapperJob, type: :job do
   describe '#perform' do
     include ActiveJob::TestHelper
     it 'gets the title' do
-      link = create(:link)
+      link = create(:link, title: nil)
       expect(link.title).to be nil
       expect(enqueued_jobs.size).to be 0
       UrlScrapperJob.perform_later link
