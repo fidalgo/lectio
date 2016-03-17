243f18d71bb7e2cfe0fcaee6694ce38afa69808b
Paulo Fidalgo <fidalgo@gmx.com>
2016-03-17 18:26:33 +0000

    Update the URLScrapperJob spec to include the new method for updating title and description.

--- spec/jobs/url_scrapper_job_spec.rb
+++ spec/jobs/url_scrapper_job_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe UrlScrapperJob, type: :job do
     it 'gets the title' do
       expect do
         link = create(:link, title: nil)
+        link.update_title_and_description
         expect(link.title).to be nil
       end.to change { enqueued_jobs.size }.by 1
     end
